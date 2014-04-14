c4878272ffbfc565a3963624918470ce3d75fc00
Leah S <lstilphen@spoonflower.com>
2014-04-14 09:28:19 -0400

    time changes

--- time.rb
+++ time.rb
@@ -4,13 +4,21 @@ puts birth
 billion = birth + 1000000000
 puts billion
 
-puts "What year were you born?"
+puts "In what month were you born?"
+month = gets.chomp
+
+puts "What day?"
+day = gets.chomp.to_i
+
+puts "What year?"
 year = gets.chomp.to_i
+
+puts "Your Birthday: #{month} #{day}, #{year}"
 
 current_year = Time.now.year
 
 age = (current_year-year)
-puts 'SPANK!'*age
+puts 'SPANK!'* age
 
 
 
